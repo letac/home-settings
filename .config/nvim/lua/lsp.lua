e5e4f0ce8257526193a49edeb2a140376dc09428
Strahinja Popovic <stralep@gmail.com>
2021-12-26 22:07:05 +0000

    neovim clippy

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -120,13 +120,24 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 -- Enable the following language servers
-local servers = { "rust_analyzer", "rnix" }
+local servers = { "rnix" }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup({
         on_attach = on_attach,
         capabilities = capabilities,
     })
 end
+nvim_lsp.rust_analyzer.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+        ["rust-analyzer"] = {
+            checkOnSave = {
+                command = "clippy",
+            },
+        },
+    },
+})
 nvim_lsp.sumneko_lua.setup({
     on_attach = on_attach,
     capabilities = capabilities,
